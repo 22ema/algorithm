731aa819002eff7e66bc9f70267391e11adf68d6
22ema <yunsung9503@gmail.com>
2023-05-16 23:54:59 +0900

    update dynamic programming, stiker_9465.py

--- dynamic_programming/stiker_9465.py
+++ dynamic_programming/stiker_9465.py
@@ -49,19 +49,41 @@
 #         answer = peel_off_stiker(stikers, visited, number, answer)
 #         print(answer)
 ## 참고 코드
-t = int(input())
-for i in range(t):
-    s = list()
-    n = int(input())
-    for k in range(2):
-        s.append(list(map(int, input().split())))
-    for j in range(1, n):
-        if j == 1:
-            s[0][j] += s[1][j-1]
-            s[1][j] += s[0][j-1]
-        else:
-            s[0][j] += max(s[1][j-1], s[1][j-2])
-            s[1][j] += max(s[0][j-1], s[0][j-2])
-    print(max(s[0][n-1], s[1][n-1]))
+# t = int(input())
+# for i in range(t):
+#     s = list()
+#     n = int(input())
+#     for k in range(2):
+#         s.append(list(map(int, input().split())))
+#     for j in range(1, n):
+#         if j == 1:
+#             s[0][j] += s[1][j-1]
+#             s[1][j] += s[0][j-1]
+#         else:
+#             s[0][j] += max(s[1][j-1], s[1][j-2])
+#             s[1][j] += max(s[0][j-1], s[0][j-2])
+#     print(max(s[0][n-1], s[1][n-1]))
 
 ## 다시 한번 풀어봐야함
+
+def peel_off_stiker(stikers, number):
+    for i in range(1, number):
+        if i == 1:
+            stikers[1][i] += stikers[0][i-1]
+            stikers[0][i] += stikers[1][i-1]
+        else:
+            stikers[0][i] += max(stikers[1][i-1], stikers[1][i-2])
+            stikers[1][i] += max(stikers[0][i-1], stikers[0][i-2])
+    return max(stikers[0][number-1], stikers[1][number-1])
+
+if __name__ == "__main__":
+    test_case = int(input())
+    for i in range(0, test_case):
+        answer = 0
+        number = int(input())
+        stikers = list()
+        for j in range(0, 2):
+            stiker = list(map(int, input().split()))
+            stikers.append(stiker)
+        answer = peel_off_stiker(stikers, number)
+        print(answer)
