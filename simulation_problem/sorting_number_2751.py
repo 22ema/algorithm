b67587e8e227527ef896743cf712d4d7bd45516d
22ema <yunsung9503@gmail.com>
2023-06-07 23:36:18 +0900

    Update code

---
+++ simulation_problem/sorting_number_2751.py
@@ -0,0 +1,11 @@
+
+
+if __name__ == "__main__":
+    number_count = int(input())
+    number_list = list()
+    for i in range(0, number_count):
+        number = int(input())
+        number_list.append(number)
+    number_list = sorted(number_list,reverse=False)
+    for i in range(0, number_count):
+        print(number_list[i])
